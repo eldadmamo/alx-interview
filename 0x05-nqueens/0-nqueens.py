8b0d31d382437a62e42bb179cd1f26a624905414
eldadmamo <eldadf456@gmail.com>
2024-08-29 21:32:30 +0000

    0x05-nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,93 +1,44 @@
 #!/usr/bin/python3
-"""
-NQUEENS
-"""
+""" N queens """
 import sys
 
 
-def is_valid_position(board, row, col):
-    """check position quuen
-    """
-    b_size = len(board)
-    if sum(board[row]) or sum([board[i][col] for i in range(b_size)]) != 0:
-        return False
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-    for i, j in [(1, 1), (-1, -1), (1, -1), (-1, 1)]:
-        r, c = row, col
-        while 0 <= r + i < b_size and 0 <= c + j < b_size:
-            r, c = r + i, c + j
-            if board[r][c]:
-                return False
-    return True
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
 
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
 
-def place_next_queen(board, row):
-    """Queen placed particular position
-    """
-    st, end = 0, len(board)
-    if sum(board[row]) == 1:
-        st = board[row].index(1) + 1
-        board[row] = [0 for col in range(end)]
-
-    for col in range(st, end):
-        if is_valid_position(board, row, col):
-            board[row][col] = 1
-            return True
-    return False
+n = int(sys.argv[1])
 
 
-def solve_nqueens(board, solutions=[]):
-    """Queens problem
-
-    Args:
-        n (int): size of board
-    """
-    n = len(board)
-    row = 0
-    while row < n:
-        if place_next_queen(board, row):
-            row += 1
-        else:
-            if row - 1 < 0:
-                break
-            row -= 1
-        if row == n:
-            solutions.append([[row, board[row].index(1)] for row in range(n)])
-            row -= 1
-
-    if row == 0:
-        return
-
-    solutions.append([[row, board[row].index(1)] for row in range(n)])
-    idx = board[0].index(1)
-    if idx > -1:
-        board = [[0 for _ in range(n)] for row in range(n)]
-        board[0][idx] = 1
-        solve_nqueens(board, solutions)
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
 
 
-def run_solver(n):
-    """Runs solver
-    """
-    board = [[0 for col in range(n)] for row in range(n)]
-    solutions = []
-    solve_nqueens(board, solutions)
-    for row in solutions:
-        print(row)
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        n = int(sys.argv[1])
-        if n < 4:
-            print('N must be at least 4')
-            sys.exit(1)
-        run_solver(n)
-
-    except ValueError:
-        print('N must be a number')
-        sys.exit(1)
+solve(n)
